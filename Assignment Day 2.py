fcb8198ce907c4927821095ab464f7f032f6818c
rehan198 <90082194+rehan198@users.noreply.github.com>
2021-09-04 22:06:58 +0530

    Assignment Day 2 (Python Zero to Hero)

---
+++ Assignment Day 2.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+Q Write a programme to find the prime number 
+enter the number to programme and get the output "Prime" or "Not Prime"
+# In[3]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+
+
+# In[4]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+
+
+# In[5]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+
+
+# In[6]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+
+
+# In[7]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number") 
+
+
+# In[8]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+    
+
+
+# In[9]:
+
+
+a = int(input("Enter a number:"))
+if a > 1 :
+    for each in range(2,a):
+        if (a % each) == 0:
+            print(a,"is not a prime number")
+            break
+    else:
+        print(a,"is a prime number")
+else:
+    print(a,"is not a prime number")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
